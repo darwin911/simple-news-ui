277cb98c116c90b52e3c5be005e598d9f2e965a3
Darwin Smith <darwin911@gmail.com>
2022-01-10 15:43:16 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [newsUrl, setNewsUrl] = useState<string>("");
+  const [sourceHtml, setSourceHtml] = useState<string[]>([""]);
+  const [title, setTitle] = useState<string>("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { data } = await axios.post("http://localhost:3001/news", {
+      newsUrl,
+    });
+    if (data) {
+      setSourceHtml(data.paragraphs);
+      setTitle(data.title);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple News</h1>
+      <p>
+        Paste a link to a news site, and hit Get Outline button to see the raw
+        contents of the news article. Please allow for a few seconds in order to
+        process your request.
+      </p>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" onChange={(e) => setNewsUrl(e.target.value)} />
+        <button>Get Outline</button>
+      </form>
+
+      <article>
+        <h2>{title}</h2>
+        {sourceHtml.map((item) => (
+          <p>{item}</p>
+        ))}
+      </article>
     </div>
   );
 }
